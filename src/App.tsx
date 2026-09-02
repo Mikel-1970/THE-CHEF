644d5712d0bcd392bc07b4dac46aba8148c334a4
Mikel-1970 <meguiluz@ingitech.com>
2026-09-02 19:39:31 +0200

    fix: reiniciar scroll al navegar y volver

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { HashRouter, Route, Routes, useLocation } from 'react-router-dom';
 import { AppProvider } from './AppContext';
 import { CookPage } from './pages/CookPage';
 import { DesirePage } from './pages/DesirePage';
@@ -12,9 +13,22 @@ import { SearchPage } from './pages/SearchPage';
 import { SettingsPage } from './pages/SettingsPage';
 import { ShoppingListPage } from './pages/ShoppingListPage';
 
+function ScrollToTop() {
+  const { pathname, search, key } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+  }, [pathname, search, key]);
+
+  return null;
+}
+
 export default function App() {
   return (
     <HashRouter>
+      <ScrollToTop />
       <AppProvider>
         <Routes>
           <Route path="/" element={<HomePage />} />
