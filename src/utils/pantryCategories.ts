9c3d1f85558614b16f566ffed611bb358f9e09f8
Mikel-1970 <meguiluz@ingitech.com>
2026-09-08 13:39:10 +0200

    fix: aplicar la clasificación final de despensa y nevera

--- src/utils/pantryCategories.ts
+++ src/utils/pantryCategories.ts
@@ -1,42 +1,40 @@
 import type { IngredientInput, ShoppingListItem } from '../domain/types';
 
 export const PANTRY_CATEGORIES = [
-  'Básicos',
+  'Básicos de cocina',
   'Aceites, vinagres y grasas',
-  'Salsas, condimentos y especias',
-  'Repostería y preparados especiales',
-  'Pastas, arroces y cereales',
+  'Salsas y condimentos',
+  'Repostería, harinas y preparados especiales',
+  'Pastas, arroz y cereales',
   'Legumbres',
   'Productos envasados',
-  'Lácteos y huevos',
-  'Carnes y charcutería',
-  'Pescados y mariscos',
-  'Verduras y hortalizas',
-  'Frutas',
-  'Panadería y masas',
-  'Congelados',
-  'Bebidas',
+  'Carne y embutidos',
+  'Pescado y marisco',
+  'Verduras, hortalizas, tubérculos y frutas',
+  'Huevos y lácteos',
+  'Pan y masas',
+  'Caldos, bases y bebidas para cocinar',
+  'Productos internacionales',
   'Otros'
 ] as const;
 
 export type PantryCategory = typeof PANTRY_CATEGORIES[number];
 
 const RULES: Array<[PantryCategory, string[]]> = [
-  ['Básicos', ['sal','azucar','miel','caldo','pan rallado']],
-  ['Aceites, vinagres y grasas', ['aceite','vinagre','mantequilla','margarina','manteca']],
-  ['Salsas, condimentos y especias', ['pimienta','pimenton','oregano','tomillo','romero','comino','curry','canela','nuez moscada','mostaza','salsa','soja','perejil','albahaca','cilantro','almendra','nuez','avellana','pistacho','cacahuete']],
-  ['Repostería y preparados especiales', ['harina','chocolate','cacao','levadura','vainilla','gelatina','maicena','coco','preparado','fondant']],
-  ['Pastas, arroces y cereales', ['pasta','espagueti','macarron','arroz','cuscus','quinoa','avena','fideo','noodle','cereal']],
+  ['Básicos de cocina', ['sal','azucar','miel','pan rallado','bicarbonato']],
+  ['Aceites, vinagres y grasas', ['aceite','vinagre','mantequilla','margarina','manteca','grasa']],
+  ['Salsas y condimentos', ['pimienta','pimenton','oregano','tomillo','romero','comino','curry','canela','nuez moscada','mostaza','salsa','soja','perejil','albahaca','cilantro','almendra','nuez','avellana','pistacho','cacahuete','anacardo','semilla']],
+  ['Repostería, harinas y preparados especiales', ['harina','chocolate','cacao','levadura','vainilla','gelatina','maicena','coco','preparado','fondant','azucar glas']],
+  ['Pastas, arroz y cereales', ['pasta','espagueti','macarron','arroz','cuscus','quinoa','avena','fideo','noodle','cereal','bulgur']],
   ['Legumbres', ['lenteja','garbanzo','alubia','judia','frijol','soja seca']],
-  ['Productos envasados', ['conserva','lata','atun','bonito','maiz','tomate triturado','tomate frito','anchoa','aceituna','bote','tarro']],
-  ['Lácteos y huevos', ['leche','queso','yogur','nata','huevo','requeson','mozzarella']],
-  ['Carnes y charcutería', ['pollo','pavo','cerdo','ternera','vacuno','cordero','jamon','chorizo','salchicha','bacon','carne']],
-  ['Pescados y mariscos', ['pescado','merluza','bacalao','salmon','sardina','caballa','trucha','rape','gamba','langostino','marisco','calamar','pulpo','mejillon']],
-  ['Verduras y hortalizas', ['patata','cebolla','ajo','tomate','pimiento','calabacin','berenjena','zanahoria','lechuga','espinaca','brocoli','coliflor','puerro','apio','pepino','verdura']],
-  ['Frutas', ['manzana','pera','platano','banana','naranja','mandarina','limon','lima','fresa','frambuesa','arandano','uva','melon','sandia','mango','piña','aguacate','fruta']],
-  ['Panadería y masas', ['pan','tortilla','wrap','croissant','brioche','bollo','masa','pizza']],
-  ['Congelados', ['congelado','helado']],
-  ['Bebidas', ['agua','zumo','refresco','cerveza','vino','sidra','bebida']]
+  ['Productos envasados', ['conserva','lata','atun','bonito','maiz','tomate triturado','tomate frito','anchoa','aceituna','encurtido','bote','tarro','frasco']],
+  ['Carne y embutidos', ['pollo','pavo','cerdo','ternera','vacuno','cordero','jamon','chorizo','salchicha','bacon','carne','embutido']],
+  ['Pescado y marisco', ['pescado','merluza','bacalao','salmon','sardina','caballa','trucha','rape','gamba','langostino','marisco','calamar','pulpo','mejillon','centollo','cangrejo']],
+  ['Verduras, hortalizas, tubérculos y frutas', ['patata','batata','boniato','cebolla','ajo','tomate','pimiento','calabacin','berenjena','zanahoria','lechuga','espinaca','brocoli','coliflor','puerro','apio','pepino','verdura','hortaliza','tuberculo','manzana','pera','platano','banana','naranja','mandarina','limon','lima','fresa','frambuesa','arandano','uva','melon','sandia','mango','piña','aguacate','fruta']],
+  ['Huevos y lácteos', ['huevo','leche','queso','yogur','nata','requeson','mozzarella','kefir']],
+  ['Pan y masas', ['pan','tortilla','wrap','croissant','brioche','bollo','masa','pizza','hojaldre']],
+  ['Caldos, bases y bebidas para cocinar', ['caldo','fumet','fondo','vino','cerveza','sidra','brandy','jerez','mirin','agua de coco']],
+  ['Productos internacionales', ['miso','tahini','harissa','gochujang','kimchi','sriracha','wasabi','nori','wakame','tortilla de maiz','curry paste','leche de coco']]
 ];
 
 export function inferPantryCategory(name: string): PantryCategory {
